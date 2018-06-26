6aefaba0b7c314684c5a17bb758e0550a8ee2b7d
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-06-26 10:54:11 +0700

    Make dashboard dark :)

---
+++ SQL/1_Dashboard_LangDetail.sql
@@ -0,0 +1,5 @@
+use CPL;
+
+insert into LangDetail values (1, 'ProfileEdit', 'Edit Profile');
+insert into LangDetail values (1, 'CredentialsEdit', 'Edit Credentials');
+insert into LangDetail values (1, 'LogOut', 'Log out');
