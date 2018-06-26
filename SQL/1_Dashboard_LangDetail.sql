bb21517481bb2b2cf202617c68f83d28321d0c96
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-06-26 08:54:11 +0700

    Clean up

--- SQL/1_Dashboard_LangDetail.sql
+++ SQL/1_Dashboard_LangDetail.sql
@@ -1,3 +1,4 @@
 use CPL;
 
-insert into LangDetail values (1, 'TokenSymbol', 'CPL')
+insert into LangDetail values (1, 'TokenSymbol', 'CPL');
+insert into LangDetail values (1, 'TokenTeam', 'CPL Team');
