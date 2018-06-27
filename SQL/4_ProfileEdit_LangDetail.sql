25b1476c46c0a0dad74458a7ade682e19112c2d9
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-06-27 13:40:04 +0700

    update view

--- SQL/4_ProfileEdit_LangDetail.sql
+++ SQL/4_ProfileEdit_LangDetail.sql
@@ -6,3 +6,8 @@ alter table SysUser add City nvarchar(30);
 alter table SysUser add StreetAddress nvarchar(100);
 alter table SysUser drop column Address;
 
+insert into LangDetail values (1, 'Name', 'Name');
+insert into LangDetail values (1, 'Gender', 'Gender');
+insert into LangDetail values (1, 'Male', 'Male');
+insert into LangDetail values (1, 'Female', 'Female');
+insert into LangDetail values (1, 'DOB', 'Date of birth');
