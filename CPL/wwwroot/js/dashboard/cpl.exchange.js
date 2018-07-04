6d46ad5b4faca3eee3a7989dca21219f1ee5491e
ngoc-dang <dangminhngoc94@gmail.com>
2018-07-04 08:21:03 +0700

    Refactor jscript

--- CPL/wwwroot/js/dashboard/cpl.exchange.js
+++ CPL/wwwroot/js/dashboard/cpl.exchange.js
@@ -1,70 +1,8 @@
 ﻿var Exchange = {
     init: function () {
-        $('section').on('click', '.btn-swap', function () {
-            //Swap label
-            if ($(this).parents("section").length > 0) {
-                var section = $(this).parents("section");
-                var label = section.find("label[for='from-amount']").text();
-                section.find("label[for='from-amount']").text(section.find("label[for='to-amount']").text());
-                section.find("label[for='to-amount']").text(label);
-
-                //Swap value
-                var value = section.find(".from-amount").val();
-                section.find(".from-amount").val(section.find(".to-amount").val());
-                section.find(".to-amount").val(value);
-
-                //Swap max amount
-                var value = section.find(".from-amount").siblings(".max-amount").val();
-                section.find(".from-amount").siblings(".max-amount").val(section.find(".to-amount").siblings(".max-amount").val());
-                section.find(".to-amount").siblings(".max-amount").val(value);
-
-                //Swap currency
-                var currency = section.find(".from-amount").siblings(".from-currency").val();
-                section.find(".from-amount").siblings(".from-currency").val(section.find(".to-amount").siblings(".to-currency").val());
-                section.find(".to-amount").siblings(".to-currency").val(currency);
-            }
-        });
-
-        $('section').on('click', '.btn-max', function () {
-            //Swap label
-            if ($(this).parents("section").length > 0) {
-                var section = $(this).parents("section");
-                //var label = section.find((".from-amount").val);
-                section.find(".from-amount").val(section.find(".from-amount").siblings(".max-amount").val()).trigger('change');
-            }
-        });
-
-        $('section').on('change paste keyup input', '.from-amount', function () {
-            if ($(this).parents("section").length > 0) {
-                var section = $(this).parents("section");
-                if (parseFloat(section.find(".from-amount").val()) > 0) {
-                    var fromCurrency = section.find(".from-amount").siblings(".from-currency").val();
-                    section.find(".invalid-amount").hide()
-                    if (fromCurrency == "BTC") {
-                        section.find(".to-amount").val(parseFloat(section.find(".from-amount").val()) * $(".btc-token-rate").val());
-                    }
-                    else if (fromCurrency == "ETH") {
-                        var btcAmount = parseFloat(section.find(".from-amount").val()) * $(".eth-btc-rate").val();
-                        section.find(".to-amount").val(btcAmount * $(".btc-token-rate").val());
-                    }
-                    else {
-                        var toCurrency = section.find(".to-amount").siblings(".to-currency").val()
-                        if (toCurrency == "BTC") {
-                            section.find(".to-amount").val(parseFloat(section.find(".from-amount").val()) / $(".btc-token-rate").val());
-                        }
-                        else if (toCurrency == "ETH") {
-                            var btcAmount = parseFloat(section.find(".from-amount").val()) / $(".btc-token-rate").val();
-                            section.find(".to-amount").val(parseFloat(btcAmount / $(".eth-btc-rate").val()));
-                        }
-                    }
-                }
-                else {
-                    $(this).parents("section").find(".invalid-amount").show();
-                    $(this).parents("section").find(".to-amount").val(null);
-                }
-            }
-        });
-
+        Exchange.bindSwap();
+        Exchange.bindMax();
+        Exchange.bindInputChange();
         Exchange.bindNext();
         Exchange.bindConfirmExchange();
     },
@@ -132,6 +70,74 @@
                 }
             });
         })
+    },
+    bindSwap: function () {
+        $('section').on('click', '.btn-swap', function () {
+            //Swap label
+            if ($(this).parents("section").length > 0) {
+                var section = $(this).parents("section");
+                var label = section.find("label[for='from-amount']").text();
+                section.find("label[for='from-amount']").text(section.find("label[for='to-amount']").text());
+                section.find("label[for='to-amount']").text(label);
+
+                //Swap value
+                var value = section.find(".from-amount").val();
+                section.find(".from-amount").val(section.find(".to-amount").val());
+                section.find(".to-amount").val(value);
+
+                //Swap max amount
+                var value = section.find(".from-amount").siblings(".max-amount").val();
+                section.find(".from-amount").siblings(".max-amount").val(section.find(".to-amount").siblings(".max-amount").val());
+                section.find(".to-amount").siblings(".max-amount").val(value);
+
+                //Swap currency
+                var currency = section.find(".from-amount").siblings(".from-currency").val();
+                section.find(".from-amount").siblings(".from-currency").val(section.find(".to-amount").siblings(".to-currency").val());
+                section.find(".to-amount").siblings(".to-currency").val(currency);
+            }
+        });
+    },
+    bindMax: function () {
+        $('section').on('click', '.btn-max', function () {
+            //Swap label
+            if ($(this).parents("section").length > 0) {
+                var section = $(this).parents("section");
+                //var label = section.find((".from-amount").val);
+                section.find(".from-amount").val(section.find(".from-amount").siblings(".max-amount").val()).trigger('change');
+            }
+        });
+    },
+    bindInputChange: function(){
+        $('section').on('change paste keyup input', '.from-amount', function () {
+            if ($(this).parents("section").length > 0) {
+                var section = $(this).parents("section");
+                if (parseFloat(section.find(".from-amount").val()) > 0) {
+                    var fromCurrency = section.find(".from-amount").siblings(".from-currency").val();
+                    section.find(".invalid-amount").hide()
+                    if (fromCurrency == "BTC") {
+                        section.find(".to-amount").val(parseFloat(section.find(".from-amount").val()) * $(".btc-token-rate").val());
+                    }
+                    else if (fromCurrency == "ETH") {
+                        var btcAmount = parseFloat(section.find(".from-amount").val()) * $(".eth-btc-rate").val();
+                        section.find(".to-amount").val(btcAmount * $(".btc-token-rate").val());
+                    }
+                    else {
+                        var toCurrency = section.find(".to-amount").siblings(".to-currency").val()
+                        if (toCurrency == "BTC") {
+                            section.find(".to-amount").val(parseFloat(section.find(".from-amount").val()) / $(".btc-token-rate").val());
+                        }
+                        else if (toCurrency == "ETH") {
+                            var btcAmount = parseFloat(section.find(".from-amount").val()) / $(".btc-token-rate").val();
+                            section.find(".to-amount").val(parseFloat(btcAmount / $(".eth-btc-rate").val()));
+                        }
+                    }
+                }
+                else {
+                    $(this).parents("section").find(".invalid-amount").show();
+                    $(this).parents("section").find(".to-amount").val(null);
+                }
+            }
+        });
     }
 }
 
