5129e5a834c212275b36815d12544c5c6092c1ce
Nhat Nguyen <quangnhat91@outlook.com>
2018-07-04 13:36:44 +0700

    Update InitDB

--- SQL/1_Update_Exchange_LangDetail.sql
+++
@@ -1,3 +0,0 @@
-update LangDetail set value = 'Calculator' where name = 'Calculator'
-INSERT [dbo].[LangDetail] ([LangId], [Name], [Value]) VALUES (1, N'TokenToBTCPrice', N'1 CPL')
-INSERT [dbo].[LangDetail] ([LangId], [Name], [Value]) VALUES (1, N'TokenToETHPrice', N'1 CPL')
