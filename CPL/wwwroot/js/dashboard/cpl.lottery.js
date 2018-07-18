ad8dc3e6431c5f4a1640dd30e93acd7b060e246f
Nhat Nguyen <quangnhat91@outlook.com>
2018-07-18 13:03:39 +0700

    [Lottery][Index] Invalid amount of ticket for user to buy

--- CPL/wwwroot/js/dashboard/cpl.lottery.js
+++ CPL/wwwroot/js/dashboard/cpl.lottery.js
@@ -6,30 +6,38 @@
     bindPurchaseTicket: function () {
         $('#form-purchase-lottery').on('click', '#btn-purchase-lottery', function () {
             var _this = this;
-            $.ajax({
-                url: "/Lottery/GetConfirmPurchaseTicket/",
-                type: "GET",
-                beforeSend: function () {
-                    $("#btn-purchase-lottery").attr("disabled", true);
-                    $("#btn-purchase-lottery").html("<i class='fa fa-spinner fa-spin'></i> <i class='fas fa-money-bill-alt'></i> " + $(_this).text().trim());
-                },
-                data: {
-                    amount: parseInt($("#number-of-ticket").val())
-                },
-                success: function (data) {
-                    if (data.url == null) {
-                        $("#modal").html(data);
-                        $("#purchase-lottery-ticket").modal("show");
+            var isFromValid = $('#form-purchase-lottery').valid();
+            $("#number-of-ticket-error").addClass("text-danger");
+            var _numberOfTickets = parseInt($("#number-of-ticket").val());
+            if (_numberOfTickets < 0 || _numberOfTickets > 5000) {
+                return false;
+            }
+            else {
+                $.ajax({
+                    url: "/Lottery/GetConfirmPurchaseTicket/",
+                    type: "GET",
+                    beforeSend: function () {
+                        $("#btn-purchase-lottery").attr("disabled", true);
+                        $("#btn-purchase-lottery").html("<i class='fa fa-spinner fa-spin'></i> <i class='fas fa-money-bill-alt'></i> " + $(_this).text().trim());
+                    },
+                    data: {
+                        amount: _numberOfTickets
+                    },
+                    success: function (data) {
+                        if (data.url == null) {
+                            $("#modal").html(data);
+                            $("#purchase-lottery-ticket").modal("show");
+                        }
+                        else {
+                            window.location.replace(data.url);
+                        }
+                    },
+                    complete: function (data) {
+                        $(_this).attr("disabled", false);
+                        $(_this).html("<i class='fas fa-money-bill-alt'></i> " + $(_this).text().trim());
                     }
-                    else {
-                        window.location.replace(data.url);
-                    }
-                },
-                complete: function (data) {
-                    $(_this).attr("disabled", false);
-                    $(_this).html("<i class='fas fa-money-bill-alt'></i> " + $(_this).text().trim());
-                }
-            });
+                });
+            };
         })
     },
     bindConfirmPurchaseTicket: function () {
