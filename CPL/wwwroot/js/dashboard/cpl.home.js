94464cb230716343564630f5475c43323fb40303
Nhat Nguyen <quangnhat91@outlook.com>
2018-08-01 17:05:51 +0700

    toppage slider

---
+++ CPL/wwwroot/js/dashboard/cpl.home.js
@@ -0,0 +1,32 @@
+﻿var Home = {
+    init: function () {
+        Home.loadSlider();
+    },
+    loadSlider: function () {
+        $("#slider-banner .owl-carousel").owlCarousel({
+            items: 2,
+            loop: true,
+            center: true,
+            merge: true,
+            margin: 25,
+            autoplay: true,
+            smartSpeed: 1000,
+            responsive: {
+            }, 
+        });
+        $("#slider-game .owl-carousel").owlCarousel({
+            items: 4,
+            loop: true,
+            margin: 5,
+            autoplay: true,
+            smartSpeed: 1000,
+            responsive: {
+            },
+        });
+    }
+}
+
+
+$(document).ready(function () {
+    Home.init();
+});
