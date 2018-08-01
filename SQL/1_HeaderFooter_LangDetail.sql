39ba1d2204a4898b6ace2cfc1f959af43afe1cc7
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-08-01 10:59:11 +0700

    update SQL

--- SQL/1_HeaderFooter_LangDetail.sql
+++
@@ -1,6 +0,0 @@
-﻿insert into LangDetail values (1, N'Deposit', N'Deposit');
-insert into LangDetail values (1, N'Deposit', N'保証金');
-insert into LangDetail values (1, N'Withdraw', N'Withdraw');
-insert into LangDetail values (1, N'Withdraw', N'撤退');
-insert into LangDetail values (1, N'AllRightReserved', 'All rights reserved');
-insert into LangDetail values (1, N'AllRightReserved', 'All rights reserved');
