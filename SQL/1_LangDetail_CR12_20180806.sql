c5c1fa1fd0ff01e2d590c5acb775bbaddbce0713
Nhat Nguyen <quangnhat91@outlook.com>
2018-08-07 13:25:57 +0700

    add multilanguage for toastr error qr code,   not allow user to withdraw 0 BTC / ETH

---
+++ SQL/1_LangDetail_CR12_20180806.sql
@@ -0,0 +1,7 @@
+﻿Use CPL;
+
+Insert Into LangDetail Values (1, N'GeneratedQRCodeError', N'Invalid QR Code.');
+Insert Into LangDetail Values (2, N'GeneratedQRCodeError', N'無効なQRコード。');
+
+Insert Into LangDetail Values (1, N'InvalidWithdrawAmount', N'Input amount need to be larger than 0.');
+Insert Into LangDetail Values (2, N'InvalidWithdrawAmount', N'入力量は0より大きくする必要があります。');
