61feb2d0a30077f25d5939e4474be979b2f91b9d
Nhat Nguyen <quangnhat91@outlook.com>
2018-08-09 16:09:50 +0700

    update border

--- CPL/wwwroot/js/dashboard/cpl.alllotterygame.js
+++ CPL/wwwroot/js/dashboard/cpl.alllotterygame.js
@@ -63,21 +63,21 @@
                 {
                     "data": "SlideImage",
                     "render": function (data, type, full, meta) {
-                        return "<a data-toggle='lightbox' data-gallery='document-" + full.id + "' href='/images/lottery/" + full.slideImage + "' ><img src=/images/lottery/" + full.slideImage + " class='img-thumbnail img-fluid' alt='document-" + full.slideImage + "'> </a>";
+                        return "<a data-toggle='lightbox' data-gallery='document-" + full.id + "' href='/images/lottery/" + full.slideImage + "' ><img src=/images/lottery/" + full.slideImage + " class='img-thumbnail img-fluid border-0' alt='document-" + full.slideImage + "'> </a>";
                     },
                     "orderable": false
                 },
                 {
                     "data": "DesktopListingImage",
                     "render": function (data, type, full, meta) {
-                        return "<a data-toggle='lightbox' data-gallery='document-" + full.id + "'  href='/images/lottery/" + full.desktopListingImage + "' ><img src=/images/lottery/" + full.desktopListingImage + " class='img-thumbnail img-fluid' alt='document-" + full.desktopListingImage + "'> </a>";
+                        return "<a data-toggle='lightbox' data-gallery='document-" + full.id + "'  href='/images/lottery/" + full.desktopListingImage + "' ><img src=/images/lottery/" + full.desktopListingImage + " class='img-thumbnail img-fluid border-0' alt='document-" + full.desktopListingImage + "'> </a>";
                     },
                     "orderable": false
                 },
                 {
                     "data": "MobileListingImage",
                     "render": function (data, type, full, meta) {
-                        return "<a data-toggle='lightbox' data-gallery='document-" + full.id + "'  href='/images/lottery/" + full.mobileListingImage + "' ><img src=/images/lottery/" + full.mobileListingImage + " class='img-thumbnail img-fluid' alt='document-" + full.mobileListingImage + "'> </a>";
+                        return "<a data-toggle='lightbox' data-gallery='document-" + full.id + "'  href='/images/lottery/" + full.mobileListingImage + "' ><img src=/images/lottery/" + full.mobileListingImage + " class='img-thumbnail img-fluid border-0' alt='document-" + full.mobileListingImage + "'> </a>";
                     },
                     "orderable": false
                 },
