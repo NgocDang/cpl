b777ea0185c2b77d1dec3d73608d8e373c6796bd
ngoc-dang <dangminhngoc94@gmail.com>
2018-08-09 15:18:49 +0700

    Create function to init tinyMCE editor

--- CPL/wwwroot/js/dashboard/cpl.news.js
+++ CPL/wwwroot/js/dashboard/cpl.news.js
@@ -26,17 +26,7 @@
                     $("#update-news").modal("show");
                     $("#update-news #btn-submit").hide();
                     tinymce.remove();
-                    tinymce.init({
-                        selector: '.tinymce',
-                        height: 300,
-                        plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker imagetools  contextmenu colorpicker textpattern help',
-                        toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
-                        image_advtab: true,
-                        content_css: [
-                            '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-                            '//www.tinymce.com/css/codepen.min.css'
-                        ]
-                    });
+                    News.initTinyMCE();
                 },
                 complete: function (data) {
                     $(_this).attr("disabled", false);
@@ -62,17 +52,7 @@
                     $("#update-news").modal("show");
                     $("#update-news #btn-save").hide();
                     tinymce.remove();
-                    tinymce.init({
-                        selector: '.tinymce',
-                        height: 300,
-                        plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker imagetools  contextmenu colorpicker textpattern help',
-                        toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
-                        image_advtab: true,
-                        content_css: [
-                            '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-                            '//www.tinymce.com/css/codepen.min.css'
-                        ]
-                    });
+                    News.initTinyMCE();
                 },
                 complete: function (data) {
                     $(_this).attr("disabled", false);
@@ -251,6 +231,19 @@
                 });
                 return false;
             }
+        });
+    },
+    initTinyMCE: function () {
+        tinymce.init({
+            selector: '.tinymce',
+            height: 300,
+            plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker imagetools  contextmenu colorpicker textpattern help',
+            toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+            image_advtab: true,
+            content_css: [
+                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                '//www.tinymce.com/css/codepen.min.css'
+            ]
         });
     }
 }
