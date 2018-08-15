f74a6e819f8e2cf15dd7127ec5bd980d6e6c4479
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-08-15 09:27:37 +0700

    Update

--- SQL/1_UpdateUX_20180814.sql
+++ SQL/1_UpdateUX_20180814.sql
@@ -4,3 +4,4 @@ insert into LangDetail values (1, N'LotteryTabImage', N'lottery_en.png');
 insert into LangDetail values (2, N'LotteryTabImage', N'lottery_jp.png');
 
 insert into LangDetail values (1, N'PricePerOneTicket', N'Price per one ticket');
+insert into LangDetail values (1, N'TotalAmount', N'Total amount');
