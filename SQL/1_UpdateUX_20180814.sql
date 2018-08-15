54730cdff365c1b37b7ea5b6ed7a049b6737fead
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-08-15 15:03:14 +0700

    Add coming soon lottery on homepage

--- SQL/1_UpdateUX_20180814.sql
+++ SQL/1_UpdateUX_20180814.sql
@@ -5,3 +5,4 @@ insert into LangDetail values (2, N'LotteryTabImage', N'lottery_jp.png');
 
 insert into LangDetail values (1, N'PricePerOneTicket', N'Price per one ticket');
 insert into LangDetail values (1, N'TotalAmount', N'Total amount');
+insert into LangDetail values (1, N'ComingSoon', N'ComingSoon');
