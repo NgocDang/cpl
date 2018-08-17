27365944463334d625b858f35e10b11cde7d566a
Nhat Nguyen <quangnhat91@outlook.com>
2018-08-17 14:11:07 +0700

    show charts on game history when user is new

--- CPL/wwwroot/js/dashboard/cpl.gamehistory.js
+++ CPL/wwwroot/js/dashboard/cpl.gamehistory.js
@@ -61,30 +61,52 @@
                     var invest = { data: [], name: 'Monthly Invests', color: '#f7931a' };
                     var prize = { data: [], name: 'Prizes', color: '#828384' };
 
-                    $.each(JSON.parse(a).AssetChange, function (index, value) {
-                        date = new Date(value.Date);
+                    if (JSON.parse(a).AssetChange.length != 0) {
+                        $.each(JSON.parse(a).AssetChange, function (index, value) {
+                            date = new Date(value.Date);
+                            now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+                            val = value.Amount;
+                            asset.data.push([now_utc, val]);
+                        });
+                    }
+                    else {
+                        date = new Date();
                         now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
-                        val = value.Amount;
+                        val = 0;
                         asset.data.push([now_utc, val]);
-                    });
+                    }
                     asset.data.sort();
 
-                    $.each(JSON.parse(a).MonthlyInvest, function (index, value) {
-                        date = new Date(value.Date);
+                    if (JSON.parse(a).MonthlyInvest.length != 0) {
+                        $.each(JSON.parse(a).MonthlyInvest, function (index, value) {
+                            date = new Date(value.Date);
+                            now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+                            val = value.Amount;
+                            invest.data.push([now_utc, val]);
+                        });
+                    }
+                    else {
+                        date = new Date();
                         now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
-                        val = value.Amount;
-
+                        val = 0;
                         invest.data.push([now_utc, val]);
-                    });
+                    }
                     invest.data.sort();
 
-                    $.each(JSON.parse(a).BonusChange, function (index, value) {
-                        date = new Date(value.Date);
+                    if (JSON.parse(a).BonusChange.length != 0) {
+                        $.each(JSON.parse(a).BonusChange, function (index, value) {
+                            date = new Date(value.Date);
+                            now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+                            val = value.Amount;
+                            prize.data.push([now_utc, val]);
+                        });
+                    }
+                    else {
+                        date = new Date();
                         now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
-                        val = value.Amount;
-
+                        val = 0;
                         prize.data.push([now_utc, val]);
-                    });
+                    }
                     prize.data.sort();
 
                     // Push the completed series
@@ -104,52 +126,58 @@
             success: function (data) {
                 if (data.success) {
                     var a = data.message;
-                    Highcharts.chart('holding-percentage-chart', {
-                        chart: {
-                            plotBackgroundColor: null,
-                            plotBorderWidth: null,
-                            plotShadow: false,
-                            type: 'pie'
-                        },
-                        title: {
-                            text: null
-                        },
-                        tooltip: {
-                            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                        },
-                        exporting: {
-                            enabled: false
-                        },
-                        plotOptions: {
-                            pie: {
-                                allowPointSelect: true,
-                                cursor: 'pointer',
-                                dataLabels: {
-                                    enabled: false
-                                },
-                                showInLegend: true
-                            }
-                        },
-                        series: [{
-                            name: 'Balance',
-                            colorByPoint: true,
-                            data: [{
-                                name: 'CPL',
-                                y: JSON.parse(a).CPLPercentage,
-                                sliced: true,
-                                selected: true,
-                                color: '#4267b2'
-                            }, {
-                                name: 'BTC',
-                                y: JSON.parse(a).BTCPercentage,
-                                color: '#f7931a'
-                            }, {
-                                name: 'ETH',
-                                y: JSON.parse(a).ETHPercentage,
-                                color: '#828384'
+                    if (JSON.parse(a).CPLPercentage != 0 || JSON.parse(a).BTCPercentage != 0 || JSON.parse(a).ETHPercentage != 0) {
+                        Highcharts.chart('holding-percentage-chart', {
+                            chart: {
+                                plotBackgroundColor: null,
+                                plotBorderWidth: null,
+                                plotShadow: false,
+                                type: 'pie'
+                            },
+                            title: {
+                                text: null
+                            },
+                            tooltip: {
+                                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                            },
+                            exporting: {
+                                enabled: false
+                            },
+                            plotOptions: {
+                                pie: {
+                                    allowPointSelect: true,
+                                    cursor: 'pointer',
+                                    dataLabels: {
+                                        enabled: false
+                                    },
+                                    showInLegend: true
+                                }
+                            },
+                            series: [{
+                                name: 'Balance',
+                                colorByPoint: true,
+                                data: [{
+                                    name: 'CPL',
+                                    y: JSON.parse(a).CPLPercentage,
+                                    sliced: true,
+                                    selected: true,
+                                    color: '#4267b2'
+                                }, {
+                                    name: 'BTC',
+                                    y: JSON.parse(a).BTCPercentage,
+                                    color: '#f7931a'
+                                }, {
+                                    name: 'ETH',
+                                    y: JSON.parse(a).ETHPercentage,
+                                    color: '#828384'
+                                }]
                             }]
-                        }]
-                    });
+                        });
+                        $("#holding-percentage-chart").removeClass("d-none");
+                    }
+                    else {
+                        $("#holding-percentage-no-assets").removeClass("d-none");
+                    }
                 }
             }
         });
