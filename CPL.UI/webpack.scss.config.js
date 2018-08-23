032a1ec00e27e1afdd1e805387836beb996b2657
DESKTOP-UJG4DC9\lan-do <bdo@netsservices.dk>
2018-08-23 13:18:11 +0700

    Done scss and ts for CPL.UI

---
+++ CPL.UI/webpack.scss.config.js
@@ -0,0 +1,33 @@
+﻿const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var _scssPath = path.resolve(__dirname, 'src/scss/dashboard/');
+var _cssPath = 'css/dashboard/';
+
+var scss = function (_fileName) {
+    return {
+        entry: path.resolve(_scssPath, _fileName + '.scss'),
+        output: {
+            filename: _cssPath + _fileName + '.css'
+        },
+        module: {
+            rules: [{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin(_cssPath + _fileName + '.css')
+        ],
+        mode: 'production',
+        stats: { colors: true }
+    };
+}
+
+module.exports = [
+    scss('cpl.layout'),
+    scss('cpl.admin')
+];
---
+++ CPL.UI/webpack.ts.config.js
@@ -0,0 +1,32 @@
+﻿const path = require('path');
+
+var _tsPath = path.resolve(__dirname, 'src/ts/dashboard/');
+var _jsPath = 'js/dashboard/';
+
+var ts = function (_fileName) {
+    return {
+        entry: path.resolve(_tsPath, _fileName + '.ts'),
+        output: {
+            filename: _jsPath + _fileName + '.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        mode: 'production',
+        stats: { colors: true }
+    };
+}
+
+module.exports = [
+    ts('cpl.layout'),
+    ts('cpl.admin')
+];
