a5648f9c75ed8a068861b790ac176fd2dc2727e3
dam-tran <tran.quoc.dam.0606@gmail.com>
2018-09-11 17:10:57 +0700

    Add purchased lottery history

---
+++ CPL/Models/PurchasedLotteryHistoryViewComponentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPL.Models
+{
+    public class PurchasedLotteryHistoryViewComponentViewModel
+    {
+        public string UserName { get; set; }
+        public string Status { get; set; }
+        public int NumberOfTicket { get; set; }
+        public decimal TotalPurchasePrice { get; set; }
+        public string Title { get; set; }
+        public DateTime PurchaseDateTime { get; set; }
+    }
+}
---
+++ CPL/ViewComponents/PurchasedLotteryHistoryViewComponent.cs
@@ -0,0 +1,54 @@
+﻿using CPL.Common.Enums;
+using CPL.Core.Interfaces;
+using CPL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CPL.Common.Enums.CPLConstant;
+
+namespace CPL.ViewComponents
+{
+    public class PurchasedLotteryHistoryViewComponent : ViewComponent
+    {
+        private readonly ILotteryCategoryService _lotteryCategoryService;
+        private readonly ILotteryService _lotteryService;
+        private readonly ILotteryHistoryService _lotteryHistoryService;
+
+
+
+        public PurchasedLotteryHistoryViewComponent(
+            ILotteryCategoryService lotteryCategoryService,
+            ILotteryService lotteryService,
+            ILotteryHistoryService lotteryHistoryService)
+        {
+            this._lotteryCategoryService = lotteryCategoryService;
+            this._lotteryService = lotteryService;
+            this._lotteryHistoryService = lotteryHistoryService;
+        }
+
+        public IViewComponentResult Invoke(int? lotteryCategoryId)
+        {
+            var lotteryCategory = _lotteryCategoryService.Queryable().FirstOrDefault(x => x.Id == lotteryCategoryId);
+            var purchasedLotteryHistory = _lotteryHistoryService
+                .Query()
+                .Include(x => x.Lottery)
+                .Include(x => x.SysUser)
+                .Select()
+                .Where(x => !lotteryCategoryId.HasValue || x.Lottery.LotteryCategoryId == lotteryCategoryId)
+                .GroupBy(x => new { x.CreatedDate , x.LotteryId, x.SysUser.Email })
+                .Select(y => new PurchasedLotteryHistoryViewComponentViewModel
+                {
+                    UserName = y.Key.Email,
+                    Status = ((EnumLotteryGameStatus)(y.FirstOrDefault().Lottery.Status)).ToString(),
+                    NumberOfTicket = y.Count(),
+                    TotalPurchasePrice = y.Sum(x => x.Lottery.UnitPrice),
+                    Title = y.FirstOrDefault().Lottery.Title,
+                    PurchaseDateTime = y.Key.CreatedDate
+                }).ToList();
+
+            return View();
+        }
+    }
+}
