4064d3bd9c4d62ec94aeabd797338e889072b6b4
dam-tran <tran.quoc.dam.0606@gmail.com>
2018-09-11 12:58:04 +0700

    add sql

--- SQL/1_LangDetail_20180911.sql
+++ SQL/1_LangDetail_20180911.sql
@@ -1,1 +1,15 @@
--- Please use this file to add lang detail for your feature implementation
+﻿-- Please use this file to add lang detail for your feature implementation
+
+-- 20180911-LotteryGame---
+ALTER TABLE LOTTERY
+ADD IsDeleted BIT NOT NULL default(0)
+
+INSERT INTO LangDetail VALUES (1, N'DeactivateSuccessfully', N'Deactivate Successfully !')
+INSERT INTO LangDetail VALUES (2, N'DeactivateSuccessfully', N'正常に終了する!')
+
+INSERT INTO LangDetail VALUES (1, N'Deactivated', N'Deactivated')
+INSERT INTO LangDetail VALUES (2, N'Deactivated', N'無効化された')
+
+INSERT INTO LangDetail VALUES (1, N'Deactivate', N'Deactivate')
+INSERT INTO LangDetail VALUES (2, N'Deactivate', N'無効化する')
+-------------------------------------------------------------------------------------------
