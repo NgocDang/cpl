21932530c1d5c931891f9656f2fe5b1aeef3e778
dam-tran <tran.quoc.dam.0606@gmail.com>
2018-09-12 10:06:40 +0700

    add sql langdetail

---
+++ SQL/5_AdminLottery_LangDetail_20180912.sql
@@ -0,0 +1,10 @@
+USE CPL;
+
+INSERT INTO LangDetail VALUES (1, N'NumberOfTicket', N'Number Of Ticket');
+INSERT INTO LangDetail VALUES (2, N'NumberOfTicket', N'チケットの数');
+
+INSERT INTO LangDetail VALUES (1, N'TotalPurchasePrice', N'Total purchase price');
+INSERT INTO LangDetail VALUES (2, N'TotalPurchasePrice', N'総購入価格');
+
+INSERT INTO LangDetail VALUES (1, N'PurchaseDateTime', N'Purchase date time');
+INSERT INTO LangDetail VALUES (2, N'PurchaseDateTime', N'購入日時');
