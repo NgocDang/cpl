60016e2d7b08b523c9ca36c7e77ee5941149479e
Nhat Nguyen <quangnhat91@outlook.com>
2018-09-20 16:26:21 +0700

    remove lotteryhistory viewComponent

--- CPL/wwwroot/js/dashboard/cpl.admin.game.js
+++ CPL/wwwroot/js/dashboard/cpl.admin.game.js
@@ -206,8 +206,8 @@
                 AdminGameManagement.loadLotterySummaryStatistics();
             }
 
-            if ($("#lottery-summary-nav .purchased-lottery-summary-history").html().trim().length <= 1300) {
-                PurchasedLotteryHistoryViewComponent.loadLotteryHistoryDataTable("#lottery-summary-nav", null);
+            if ($("#lottery-summary-nav .purchased-lottery-summary-history").html().trim().length <= 2100) {
+                AdminGameManagement.loadLotteryHistoryDataTable("#lottery-summary-nav", null);
             }
             
         });
@@ -364,8 +364,8 @@
                 AdminGameManagement.loadLotteryCategoryStatistics("#lottery-category-nav-" + $(_this).data().lotteryCategoryId, $(_this).data().lotteryCategoryId);
             }
 
-            if ($("#lottery-category-nav-" + $(_this).data().lotteryCategoryId + " .purchased-lottery-category-history").html().trim().length <= 1300) {
-                PurchasedLotteryHistoryViewComponent.loadLotteryHistoryDataTable("#lottery-category-nav-" + $(_this).data().lotteryCategoryId, $(_this).data().lotteryCategoryId);
+            if ($("#lottery-category-nav-" + $(_this).data().lotteryCategoryId + " .purchased-lottery-category-history").html().trim().length <= 2200) {
+                AdminGameManagement.loadLotteryHistoryDataTable("#lottery-category-nav-" + $(_this).data().lotteryCategoryId, $(_this).data().lotteryCategoryId);
             }
         });
     },
@@ -515,7 +515,82 @@
                 var _this = this;
                 AdminGameManagement.loadLotteryCategoryStatistics("#lottery-category-nav-" + $(_this).data().lotteryCategoryId, $(_this).data().lotteryCategoryId);
             });
-    }
+    },
+    loadLotteryHistoryDataTable: function (parentElement, lotteryCategoryId) {
+        return $(parentElement + " .dt-purchased-lottery-history").DataTable({
+            "processing": true,
+            "serverSide": true,
+            "autoWidth": false,
+            "ajax": {
+                url: "/Admin/SearchPurchasedLotteryHistory",
+                type: 'POST',
+                data: {
+                    lotteryCategoryId: lotteryCategoryId
+                }
+            },
+            "language": DTLang.getLang(),
+            "columns": [
+                {
+                    "data": "UserName",
+                    "render": function (data, type, full, meta) {
+                        return full.userName;
+                    }
+                },
+                {
+                    "data": "StatusInString",
+                    "render": function (data, type, full, meta) {
+                        if (full.status == 1) {
+                            return "<p class='text-sm-center'><span class='badge badge-info'>" + $("#pending").val() + "</span></p>";
+                        }
+                        else if (full.status == 2) {
+                            return "<p class='text-sm-center'><span class='badge badge-success'>" + $("#active").val() + "</span></p>";
+                        }
+                        else if (full.status == 3) {
+                            return "<p class='text-sm-center'><span class='badge badge-secondary'>" + $("#completed").val() + "</span></p>";
+                        }
+                        else if (full.status == 4) {
+                            return "<p class='text-sm-center'><span class='badge badge-warning'>" + $("#deactivated").val() + "</span></p>";
+                        }
+                        else {
+                            return "";
+                        }
+                    }
+                },
+                {
+                    "data": "NumberOfTicket",
+                    "render": function (data, type, full, meta) {
+                        return full.numberOfTicket;
+                    }
+                },
+                {
+                    "data": "NumberOfTicketInString",
+                    "render": function (data, type, full, meta) {
+                        return full.totalPurchasePrice;
+                    }
+                },
+                {
+                    "data": "Title",
+                    "render": function (data, type, full, meta) {
+                        return full.title;
+                    }
+                },
+                {
+                    "data": "PurchaseDateTimeInString",
+                    "render": function (data, type, full, meta) {
+                        return full.purchaseDateTimeInString;
+                    }
+                },
+                {
+                    "data": "Details",
+                    "render": function (data, type, full, meta) {
+                        var html = "<a style='line-height:12px;margin:2px' href='/Admin/User/" + full.sysUserId + "' target='_blank' class='btn btn-sm btn-outline-secondary btn-view'>" + $("#view").val() + "</a>";
+                        return html;
+                    },
+                    "orderable": false
+                }
+            ],
+        });
+    },
 }
 
 $(document).ready(function () {
