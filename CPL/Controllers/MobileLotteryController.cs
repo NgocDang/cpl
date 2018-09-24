393ad1843018d1185b78c218723bca2345fa89d6
Vinh Nguyễn <vinh-nguyen@famhopper.com>
2018-09-24 14:45:54 +0700

    Create API for Lottery - ConfirmPurchaseTicket - GetHomeScreenData - GetUserInfomation - GetLotteryDetail

---
+++ CPL/Controllers/MobileLotteryController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using CPL.Common.Enums;
+using CPL.Core.Interfaces;
+using CPL.Misc;
+using CPL.Misc.Enums;
+using CPL.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CPL.Controllers
+{
+    public class MobileLotteryController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IHostingEnvironment _appEnvironment;
+        private readonly ILotteryService _lotteryService;
+
+        public MobileLotteryController(
+            IMapper mapper,
+            ILotteryService lotteryService,
+            IHostingEnvironment appEnvironment
+        )
+        {
+            this._mapper = mapper;
+            this._appEnvironment = appEnvironment;
+            this._lotteryService = lotteryService;
+        }
+
+        [HttpGet]
+        [Permission(EnumRole.User)]
+        public IActionResult GetLotteryDetail(MobileModel mobileModel, int lotteryId)
+        {
+            try
+            {
+                var lottery = _lotteryService
+                                 .Query()
+                                 .Include(x => x.LotteryHistories)
+                                 .Include(x => x.LotteryDetails)
+                                 //.Include(x => x.LotteryPrizes)
+                                 .Select()
+                                 .FirstOrDefault(x => x.Id == lotteryId && !x.IsDeleted && (x.Status == (int)EnumLotteryGameStatus.ACTIVE || x.Status == (int)EnumLotteryGameStatus.DEACTIVATED));
+
+                if (lottery != null)
+                {
+                    return new JsonResult(
+                        new
+                        {
+                            code = EnumResponseStatus.SUCCESS,
+                            data = Mapper.Map<LotteryIndexViewModel>(lottery)
+                        }
+                    );
+                }
+
+                return new JsonResult(
+                    new
+                    {
+                        code = EnumResponseStatus.WARNING,
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(
+                    new
+                    {
+                        code = EnumResponseStatus.ERROR,
+                        error_message_key = ex.Message
+                    }
+                );
+            }
+        }
+    }
+}
