abb59fc6084b10eb97d2821bcfb52d11f5e19497
dam-tran <tran.quoc.dam.0606@gmail.com>
2018-10-01 09:12:17 +0700

    Add LangDetail for What is Crypto odds

--- SQL/01_LangDetail_20180928.sql
+++ SQL/01_LangDetail_20180928.sql
@@ -13,4 +13,17 @@ insert into LangDetail values (2, N'WhatIsBitcoin', N'ビットコインとは')
 insert into LangDetail values (1, N'PersonalInformationProtection', N'Personal information protection');
 insert into LangDetail values (2, N'PersonalInformationProtection', N'個人情報保護について');
 
-select * from LangDetail where Name like '%About%'
+--- DamTran - What is Crypto Odds - 2018/10/01
+USE CPL
+
+INSERT INTO LangDetail VALUES (1, N'WhatIsCryptoodds', N'What is Crypto Odds');
+INSERT INTO LangDetail VALUES (2, N'WhatIsCryptoodds', N'クリプトオッズとは');
+
+INSERT INTO LangMsgDetail VALUES (1, N'WhatIsCryptoodds', 
+N'Crypt odds is an online gaming site holding online gaming licenses issued by the government of the Netherlands Curacao. <br />
+Crypto odds is an on-line casino that maintains anonymity by depositing and withdrawing in virtual currency and features fast deposits and withdrawals. Please enjoy from one registration to get one rushrack with one smartphone. <br />
+We use a system that randomly generates numbers called smart contract technology and random number generator (RNG) for contents, and we provide safe and secure gaming with a system that eliminates fraud.');
+INSERT INTO LangMsgDetail VALUES (2, N'WhatIsCryptoodds',
+N'クリプトオッズはオランダ領キュラソー政府発行のオンラインゲーミングライセンスを保有するオンラインゲーミングサイトです。<br />
+クリプトオッズは仮想通貨での入出金により匿名性を保ち、迅速な入出金が特徴のオンラインカジノです。スマホ一つで登録から一攫千金までお楽しみください。<br />
+コンテンツにはスマートコントラクト技術やランダムナンバージェネレーター(RNG)と呼ばれる数字をランダムに生成するシステムを使用しており、不正を排除したシステムで安全・安心なゲーミングを提供しています。');
