a97ac8ed995fe9ba77f2358c9ab88991ec37a937
dam-tran <tran.quoc.dam.0606@gmail.com>
2018-10-01 14:29:05 +0700

    Add LangDetail

--- SQL/01_LangDetail_20180928.sql
+++ SQL/01_LangDetail_20180928.sql
@@ -27,3 +27,39 @@ INSERT INTO LangMsgDetail VALUES (2, N'WhatIsCryptoodds',
 N'クリプトオッズはオランダ領キュラソー政府発行のオンラインゲーミングライセンスを保有するオンラインゲーミングサイトです。<br />
 クリプトオッズは仮想通貨での入出金により匿名性を保ち、迅速な入出金が特徴のオンラインカジノです。スマホ一つで登録から一攫千金までお楽しみください。<br />
 コンテンツにはスマートコントラクト技術やランダムナンバージェネレーター(RNG)と呼ばれる数字をランダムに生成するシステムを使用しており、不正を排除したシステムで安全・安心なゲーミングを提供しています。');
+
+-- DamTran - Easy 3 steps - 2018/10/01
+USE CPL
+INSERT INTO LangDetail VALUES (1, N'ForEnjoyingCryptOdds', N'For enjoying crypt odds');
+INSERT INTO LangDetail VALUES (2, N'ForEnjoyingCryptOdds', N'クリプトオッズを楽しむ為の');
+
+INSERT INTO LangDetail VALUES (1, N'Easy3Steps', N'Easy · Easy 3 steps');
+INSERT INTO LangDetail VALUES (2, N'Easy3Steps', N'簡単・楽々　３ステップ');
+
+INSERT INTO LangDetail VALUES (1, N'EasyRegistration', N'Easy registration');
+INSERT INTO LangDetail VALUES (2, N'EasyRegistration', N'楽々登録');
+
+INSERT INTO LangDetail VALUES (1, N'EasyRegistrationDetail',
+ N'Identity registration absolutely unnecessary! <br />
+Easy registration with e-mail address and password setting only!');
+INSERT INTO LangDetail VALUES (2, N'EasyRegistrationDetail', 
+N'身分証登録一切不要！ <br />
+メールアドレスとパスワード設定のみで簡単登録！');
+INSERT INTO LangDetail VALUES (1, N'RegisterFor30Seconds', N'Register for 30 seconds');
+INSERT INTO LangDetail VALUES (2, N'RegisterFor30Seconds', N'３０秒登録');
+
+INSERT INTO LangDetail VALUES (1, N'BTCDeposit', N'BTC deposit');
+INSERT INTO LangDetail VALUES (2, N'BTCDeposit', N'BTC入金');
+
+INSERT INTO LangDetail VALUES (1, N'BTCDepositDetail',
+ N'Just remit BTC to the designated address! <br />
+ Automatic BTC is converted to CPO.');
+INSERT INTO LangDetail VALUES (2, N'BTCDepositDetail', 
+N'指定のアドレスにBTCを送金するだけ！<br />
+自動BTCがCPOに変換されます。');
+
+INSERT INTO LangDetail VALUES (1, N'PlayGameDetail',
+ N'Please choose your favorite game <br />Please enjoy the crypt odds as much as you want');
+INSERT INTO LangDetail VALUES (2, N'PlayGameDetail', 
+N'お好みのゲームを選択して <br />思う存分クリプトオッズをお楽しみ下さい');
+
