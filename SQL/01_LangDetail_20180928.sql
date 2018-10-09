9f45e500fc2cee5ce18440812d721ea8850c1810
dam-tran <tran.quoc.dam.0606@gmail.com>
2018-10-09 15:11:48 +0700

    Update LangDetail

--- SQL/01_LangDetail_20180928.sql
+++ SQL/01_LangDetail_20180928.sql
@@ -50,9 +50,9 @@ INSERT INTO LangDetail VALUES (1, N'WhatIsCryptoOdds', N'What is Crypto Odds');
 INSERT INTO LangDetail VALUES (2, N'WhatIsCryptoOdds', N'クリプトオッズとは');
 
 INSERT INTO LangMsgDetail VALUES (1, N'WhatIsCryptoOdds', 
-N'Crypto Odds is an online gaming site holding online gaming licenses issued by the government of the Netherlands Curacao. <br />
-Crypto Odds is an on-line casino that maintains anonymity by depositing and withdrawing in virtual currency and features fast deposits and withdrawals. Please enjoy from one registration to get one rushrack with one smartphone. <br />
-We use a system that randomly generates numbers called smart contract technology and random number generator (RNG) for contents, and we provide safe and secure gaming with a system that eliminates fraud.');
+N'Crypt Odds is an online game website holding game online license issued by the government of Netherlands Curacao. <br />
+Crypto Odds is an online casino that have anonymity in depositing and withdrawing by using cryptocurrency and fast deposit and withdrawal features. Please enjoy from one registration to get rich quick with smartphone. <br />
+In this game, we use smart contract technology and random number generator (RNG) a system that randomly generates numbers, and we provide a safe and secure game with a system that eliminates fraud.');
 INSERT INTO LangMsgDetail VALUES (2, N'WhatIsCryptoOdds',
 N'クリプトオッズはオランダ領キュラソー政府発行のオンラインゲーミングライセンスを保有するオンラインゲーミングサイトです。<br />
 クリプトオッズは仮想通貨での入出金により匿名性を保ち、迅速な入出金が特徴のオンラインカジノです。スマホ一つで登録から一攫千金までお楽しみください。<br />
@@ -60,7 +60,7 @@ N'クリプトオッズはオランダ領キュラソー政府発行のオンラ
 
 -- DamTran - Easy 3 steps - 2018/10/01
 USE CPL
-INSERT INTO LangDetail VALUES (1, N'ForEnjoyingCryptOdds', N'For enjoying Crypto Odds');
+INSERT INTO LangDetail VALUES (1, N'ForEnjoyingCryptOdds', N'To enjoy Crypto Odds');
 INSERT INTO LangDetail VALUES (2, N'ForEnjoyingCryptOdds', N'クリプトオッズを楽しむ為の');
 
 INSERT INTO LangDetail VALUES (1, N'Easy3Steps', N'3 Simple Steps');
@@ -82,7 +82,7 @@ INSERT INTO LangDetail VALUES (1, N'BTCDeposit', N'BTC Deposit');
 INSERT INTO LangDetail VALUES (2, N'BTCDeposit', N'BTC入金');
 
 INSERT INTO LangDetail VALUES (1, N'BTCDepositDetail',
- N'Just deposit BTC to the designated address! <br />
+ N'Just deposit BTC to the address provided by us! <br />
  BTC is automatically converted to CPO.');
 INSERT INTO LangDetail VALUES (2, N'BTCDepositDetail', 
 N'指定のアドレスにBTCを送金するだけ！<br />
